d3c45d0162e08c4473214b8bff13291770b2515c
vick4 <victorisaacblw@gmail.com>
2023-11-22 00:31:22 +0000

    done

--- models/user.py
+++ models/user.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-"""This is the user class"""
+'''
+    Implementation of the User class which inherits from BaseModel
+'''
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
-from sqlalchemy.orm import relationship, backref
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-
-    places = relationship("Place",
-                          backref="user",
-                          cascade="all, delete, delete-orphan")
-
-    reviews = relationship("Review",
-                           backref="user",
-                           cascade="all, delete, delete-orphan")
+    '''
+        Definition of the User class
+    '''
+    __tablename__ = "users"
+    if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user",
+                              cascade="all, delete, delete-orphan")
+        reviews = relationship("Review", backref="user",
+                               cascade="all, delete, delete-orphan")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
